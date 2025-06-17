b304ba81af3c1f5ab96b1e093b89f9e17188e4ce
Gyanendra-Tiwari <151989331+Gyanendra-Tiwari@users.noreply.github.com>
2025-06-17 11:19:15 +0530

    Update Doctor.java

--- src/main/java/com/bms/clinicmanagementsystem/model/Doctor.java
+++ src/main/java/com/bms/clinicmanagementsystem/model/Doctor.java
@@ -1,1 +1,34 @@
 
+package com.bms.clinicmanagementsystem.model;
+
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+public class Doctor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String specialization;
+    private String email;
+    private String phone;
+
+    @OneToMany(mappedBy = "doctor", cascade = CascadeType.ALL)
+    private List<Appointment> appointments;
+
+    public Doctor() {
+    }
+
+    public Doctor(String firstName, String lastName, String specialization, String email, String phone) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.specialization = specialization;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    // Getters and
